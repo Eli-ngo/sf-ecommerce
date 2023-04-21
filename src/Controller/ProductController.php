c87cfe0eb1ffb8dca52cf66a452029a27ce9e736
Elisabeth Ngo <78852168+Eli-ngo@users.noreply.github.com>
2023-04-21 09:59:23 +0200

    error pages and messages

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/', name: 'app_product')]
+    public function index(EntityManagerInterface $em): Response
     {
+        $products = $em->getRepository(Product::class)->findAll();
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
         ]);
     }
+
+    /** PAGE DETAIL PRODUIT */
+    #[Route('/product/{id}', name: 'product_detail')]
+    public function product(Product $product = null){
+
+        if($product == null){
+            return $this->redirectToRoute('app_product');
+        }
+
+        return $this->render('product/product_detail.html.twig', [
+            'product' => $product
+        ]);
+    }
+
 }
