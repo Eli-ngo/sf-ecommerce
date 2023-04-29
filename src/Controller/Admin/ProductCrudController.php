9ae87fc53295fcc60624821c7c5a9d676bd837a6
Forofoot <erwan.marques@yahoo.fr>
2023-04-29 22:43:21 +0200

    Render as html

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -9,10 +9,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use Vich\UploaderBundle\Form\Type\VichImageType;
-use Symfony\Component\HttpFoundation\Request;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -25,7 +25,7 @@ class ProductCrudController extends AbstractCrudController
     {
         return [
             TextField::new('name'),
-            TextEditorField::new('description'),
+            TextareaField::new('description')->renderAsHtml(),
             MoneyField::new('price')->setCurrency('EUR'),
             IntegerField::new('supply'),
             TextField::new('imageFile')
