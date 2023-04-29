06a5e9ed1f2a4761edb0ca7caf649f989de367a8
Forofoot <erwan.marques@yahoo.fr>
2023-04-29 18:13:54 +0200

    Add upload configurations

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,12 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -24,9 +28,18 @@ class ProductCrudController extends AbstractCrudController
             TextEditorField::new('description'),
             MoneyField::new('price')->setCurrency('EUR'),
             IntegerField::new('supply'),
+            TextField::new('imageFile')
+                ->setFormType(VichImageType::class)
+                ->setFormTypeOptions([
+                    'allow_delete' => true,
+                    'download_label' => true,
+                ])
+                ->onlyOnForms(),
             ImageField::new('image')
                 ->setBasePath('/uploads')
-                ->setUploadDir('/public/uploads')
+                ->onlyOnIndex(),
+
+
         ];
     }
 }
